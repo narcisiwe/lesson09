14d0b24d3507095587f5f47c80649ad89440135b
Vladimir Larionov <narcisiwe@gmail.com>
2020-11-18 01:54:55 +0300

    Fix: Errors#01

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const buttonCalculate = document.getElementById('start');
+console.log(buttonCalculate);
+
+
+const buttonPlusIncome = document.getElementsByTagName('button')[0];
+console.log(buttonPlusIncome);
+
+const buttonPlusExpenses = document.getElementsByTagName('button')[1];
+console.log(buttonPlusExpenses);
+
+
+const checkBox = document.querySelector('#deposit-check');
+console.log(checkBox);
+
+
+const addIncomeItems = document.querySelectorAll('.additional_income-item');
+console.log(addIncomeItems);
+
+
+const budgetMonthValue = document.getElementsByClassName('result-total budget_month-value')[0];
+console.log(budgetMonthValue);
+
+const budgetDayValue = document.getElementsByClassName('result-total budget_day-value')[0];
+console.log(budgetDayValue);
+
+const expensesMonthValue = document.getElementsByClassName('result-total expenses_month-value')[0];
+console.log(expensesMonthValue);
+
+const additionalIncomeValue = document.getElementsByClassName('result-total additional_income-value')[0];
+console.log(additionalIncomeValue);
+
+const additionalExpensesValue = document.getElementsByClassName('result-total additional_expenses-value')[0];
+console.log(additionalExpensesValue);
+
+const incomePeriodValue = document.getElementsByClassName('result-total income_period-value')[0];
+console.log(incomePeriodValue);
+
+const targetMonthValue = document.getElementsByClassName('result-total target_month-value')[0];
+console.log(targetMonthValue);
+
+
+const salaryAmount = document.querySelector('.salary-amount');
+console.log(salaryAmount);
+
+const incomeTitle = document.querySelector('.income-title');
+console.log(incomeTitle);
+
+const incomeAmount = document.querySelector('.income-amount');
+console.log(incomeAmount);
+
+const expensesTitle = document.querySelector('.expenses-title');
+console.log(expensesTitle);
+
+const expensesAmount = document.querySelector('.expenses-amount');
+console.log(expensesAmount);
+
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+console.log(additionalExpensesItem);
+
+const depositAmount = document.querySelector('.deposit-amount');
+console.log(depositAmount);
+
+const depositPercent = document.querySelector('.deposit-percent');
+console.log(depositPercent);
+
+const targetAmount = document.querySelector('.target-amount');
+console.log(targetAmount);
+
+const periodSelect = document.querySelector('.period-select');
+console.log(periodSelect);
+
